7569734dd73fd91aec7f835b32c1281956e18846
Stonyx <info@stonyx.com>
2021-10-04 22:51:53 -0400

    Modified copyright and license header in main.c file
    Per the request of the original author the copyright and license header has been modified.

--- src/main.c
+++ src/main.c
@@ -2,17 +2,8 @@
  * Copyright (C) 2021 Stonyx
  * http://www.stonyx.com
  *
- * This script is free software. You can redistribute it and/or modify it under the terms of the GNU
- * General Public License Version 3 (or at your option any later version) as published by The Free
- * Software Foundation.
- *
- * This script is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * If you did not received a copy of the GNU General Public License along with this script see
- * http://www.gnu.org/copyleft/gpl.html or write to The Free Software Foundation, 675 Mass Ave,
- * Cambridge, MA 02139, USA.
+ * Copyright (C) 2020 Guillaume Valadon
+ * guillaume@valadon.net
  */
 
 #include <stdlib.h>
